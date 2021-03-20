96bfd085e7aeec8b9b4a3901b020e7bc86903eb4
Andy123567 <andy.vandeweyer08@gmail.com>
2021-03-20 11:21:43 +0100

    Hallo

--- JavaScript/OefeningJSD2.js
+++ JavaScript/OefeningJSD2.js
@@ -1,40 +1,50 @@
 // Oefening 1
 window.onload = function (){
-    Oefening1()
+    Oefening7()
 }
-let Zin1, Zin2, Zin3;
 
-Zin1= "Dit is de eerste zin"
-Zin2= "Dit is de tweede zin"
-Zin3= "Dit is de derde zin"
+function OEFening1(){
+    let Zin1, Zin2, Zin3;
 
-document.getElementById( elementid = 'Zin1').innerHTML = Zin1;
-document.getElementById( elementid = 'Zin2').innerHTML = Zin2;
-document.getElementById( elementid = 'Zin3').innerText = Zin3;
+    Zin1= "Dit is de eerste zin"
+    Zin2= "Dit is de tweede zin"
+    Zin3= "Dit is de derde zin"
 
-console.log(Zin1);
-console.log(Zin2);
-console.log(Zin3);
+    document.getElementById( elementid = 'Zin1').innerHTML = Zin1;
+    document.getElementById( elementid = 'Zin2').innerHTML = Zin2;
+    document.getElementById( elementid = 'Zin3').innerText = Zin3;
+
+    console.log(Zin1);
+    console.log(Zin2);
+    console.log(Zin3);
+}
+
 
 // Oefening 2
-let array = ('Dit is zin 1', 'Dit is zin 2', 'Dit is zin 3');
-let output;
+function OEFening2(){
+    let array = ('Dit is zin 1', 'Dit is zin 2', 'Dit is zin 3');
+    let output;
 
-array.forEach(zin => { output += ' ' + zin });
-alert(Zin1 + ' ' + Zin2 + ' ' + Zin3);
-alert(`${Zin1} dit is een doorlopende tekst ${Zin2} en ${Zin3}`);
-alert(output);
+    array.forEach(zin => { output += ' ' + zin });
+    alert(Zin1 + ' ' + Zin2 + ' ' + Zin3);
+    alert(`${Zin1} dit is een doorlopende tekst ${Zin2} en ${Zin3}`);
+    alert(output);
+}
+
 
 // Oefening 3
-let koekje = 'Ik wil een koekje'
-console.log(koekje.charAt(0));
-console.log(koekje.charAt(3));
-console.log(koekje.lastIndexOf('k'));
-console.log(koekje.lastIndexOf('e'));
-console.log(koekje.length);
+function OEFening3(){
+    let koekje = 'Ik wil een koekje'
+    console.log(koekje.charAt(0));
+    console.log(koekje.charAt(3));
+    console.log(koekje.lastIndexOf('k'));
+    console.log(koekje.lastIndexOf('e'));
+    console.log(koekje.length);
 
-let lasagne = koekje.replace( 'koekje',  'lasagne van de pxl');
-document.getElementById( 'lasagne').innerHTML = lasagne.toUpperCase();
+    let lasagne = koekje.replace( 'koekje',  'lasagne van de pxl');
+    document.getElementById( 'lasagne').innerHTML = lasagne.toUpperCase();
+}
+
 
 function Promp1(){
     let woord = prompt ('geef een woord in');
@@ -170,7 +180,7 @@ function Oefening7(){
             result = 'Julli';
             break;
     }
-    console.log('Het is de maand' + result);
+    console.log('Het is de maand ' + result);
 }
 
 function Oefening8() {
