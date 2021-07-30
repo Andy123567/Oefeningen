bb2a251a7f0d872548030e11a6f77d70567d3f11
Andy123567 <andy.vandeweyer08@gmail.com>
2021-07-30 11:52:48 +0200

    .

---
+++ JavaScript/WebsiteKimberley/Informatie.js
@@ -0,0 +1,6 @@
+mybutton = document.getElementById("myBtn");
+
+
+document.getElementById("myBtn").onclick = function () {
+    location.href = "../../HTML/WebsiteKimberley/Main.html";
+};
---
+++ JavaScript/WebsiteKimberley/Main.js
@@ -0,0 +1,3 @@
+document.getElementById("myBtn").onclick = function () {
+    location.href = "../../HTML/WebsiteKimberley/Main.html";
+};
